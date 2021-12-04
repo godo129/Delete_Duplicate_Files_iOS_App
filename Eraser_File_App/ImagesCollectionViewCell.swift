7f6f0c617a858f95e7526ecd6849637536694f31
godo <godo129@naver.com>
2021-12-04 20:54:07 +0900

    포토 라이브러리에서 정보 가져와서 중복된 정보 들 저장 하고 테이블 뷰에 뿌려주기

---
+++ Eraser_File_App/ImagesCollectionViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  ImagesCollectionViewCell.swift
+//  Eraser_File_App
+//
+//  Created by hong on 2021/12/04.
+//
+
+import UIKit
+
+class ImagesCollectionViewCell: UICollectionViewCell {
+    var ExerciseImage: UIImageView = {
+            let ExerciseImage = UIImageView()
+            ExerciseImage.backgroundColor = .white
+            ExerciseImage.clipsToBounds = true
+            ExerciseImage.contentMode = .scaleAspectFill
+            return ExerciseImage
+        }()
+        
+        var ExerciseLabel: UILabel = {
+            let ExerciseLabel = UILabel()
+            ExerciseLabel.backgroundColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 1)
+            ExerciseLabel.textAlignment = .center
+            ExerciseLabel.textColor = .black
+            ExerciseLabel.font = .systemFont(ofSize: 20, weight: .bold)
+            return ExerciseLabel
+            
+        }()
+        
+        static let identifier = "ExerciseCollectionViewCell"
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            contentView.backgroundColor = .systemRed
+
+            contentView.addSubview(ExerciseImage)
+            contentView.addSubview(ExerciseLabel)
+            contentView.clipsToBounds = true
+
+            
+        
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+      
+        /*
+        func setUpCompo() {
+            
+            ExerciseLabel = UILabel()
+            
+            
+            ExerciseImage = UIImageView()
+            contentView.addSubview(ExerciseImage)
+            contentView.addSubview(ExerciseImage)
+            ExerciseImage.translatesAutoresizingMaskIntoConstraints = false
+            ExerciseImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+            ExerciseImage.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
+            ExerciseImage.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
+            ExerciseImage.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
+            
+        }
+        
+        func setUpLabel() {
+            ExerciseLabel.font = UIFont.systemFont(ofSize: 20)
+            ExerciseLabel.textAlignment = .center
+            
+        }
+     */
+        
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            ExerciseImage.frame = CGRect(x: 0, y: 0, width: contentView.frame.size.width, height: contentView.frame.size.height-30)
+            
+            ExerciseLabel.frame = CGRect(x: 0,
+                                         y: contentView.frame.size.height-30,
+                                         width: contentView.frame.size.width,
+                                         height: 30)
+        }
+}
--- Eraser_File_App/ViewController.swift
+++ Eraser_File_App/ViewController.swift
@@ -6,14 +6,236 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
+    
+    var fetchResult: PHFetchResult<PHAsset>!
+    
+    let tableView = UITableView()
+    
+    var imageList: [Data] = []
+    
+    var duplicateImage: [UIImage] = []
+    
+    var duplicateData: [PHAsset] = []
+    
+    let imageView = UIImageView()
+    
+    
+    
+//
+//    let layout: UICollectionViewFlowLayout = {
+//        let layout = UICollectionViewFlowLayout()
+//        layout.itemSize = CGSize(width: 150, height: 150)
+//        layout.scrollDirection = .horizontal
+//        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//        return layout
+//    }()
+//
+//    let cells = UICollectionView(frame: .zero,collectionViewLayout: UICollectionViewFlowLayout())
+//
+    
+    
+    // 썸네일 만들기
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+//
+//        cells.dataSource = self
+//        cells.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//        cells.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell2")
+        
+        view.addSubview(tableView)
+        
+        
+        view.addSubview(imageView)
+        
+ 
+        //view.addSubview(cells)
+        
+        reqeustsPhotoPermission()
+        registerPhotoLibrary()
+        
+        print(duplicateData)
+        print(duplicateImage)
+
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+        imageView.frame = CGRect(x: 200, y: 200, width: 300, height: 300)
+        
+    }
+    
+    private func reqeustsPhotoPermission() {
+        
+        
+        let photoAutorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAutorizationStatus {
+        case .authorized:
+            print("allowed")
+            self.requestCollection()
+        case .denied :
+            print("denied")
+        case .notDetermined:
+            print("Photo Authorization status is not determined")
+            
+            PHPhotoLibrary.requestAuthorization() {
+                (status) in
+                switch status {
+                case .authorized:
+                    print("User permiited.")
+                    self.requestCollection()
+                case .denied:
+                    print("User denied.")
+                    break
+                default:
+                    break
+                }
+            }
+        case .restricted :
+            print("Photo Authorization status is restricted ")
+ 
+        default:
+            break
+        }
+        
+    }
+    
+    private func requestCollection() {
+        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        
+        guard let cameraRollCollection = cameraRoll.firstObject else {
+            return
+        }
+        
+        let fetchOption = PHFetchOptions()
+        fetchOption.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOption)
+        
+        
+        
+        
+        
+        for idx in 0..<self.fetchResult.count {
+            
+            
+            let asset = fetchResult.object(at: idx)
+            
+            imageManager.requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { image, _ in
+                
+                if self.imageList.contains((image?.pngData())!) {
+                    self.imageView.image = image
+                    self.duplicateData.append(asset)
+                    self.duplicateImage.append(image!)
+                    print(idx)
+                } else {
+                    self.imageList.append((image?.pngData())!)
+                }
+
+                
+            }
+       
+        }
+        
+        
+        
+        OperationQueue.main.addOperation {
+            self.tableView.reloadData()
+        }
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fetchResult.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            fatalError("The cell name not exist.")
+        }
+        
+        guard let asset: PHAsset = self.fetchResult?.object(at: indexPath.row) else {
+            return cell
+        }
+        
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { image, _ in
+            cell.imageView?.image = image
+        }
+        
+        return cell
+    }
+    
+    // 삭제
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let asset = self.fetchResult?[indexPath.row]
+            
+            PHPhotoLibrary.shared().performChanges({PHAssetChangeRequest.deleteAssets([asset] as NSFastEnumeration)}, completionHandler: nil)
+            
+        }
+    }
+    
+}
+
+//extension ViewController:UICollectionViewDataSource,UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return self.fetchResult.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath) as! ImagesCollectionViewCell
+//
+//        guard let asset: PHAsset = self.fetchResult?.object(at: indexPath.row) else {
+//            return cell2
+//        }
+//
+//        imageManager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFill, options: nil) { image, _ in
+//            cell2.ExerciseImage.image = image
+//        }
+//
+//        return cell2
+//    }
+//
+//
+//}
+
+extension ViewController: PHPhotoLibraryChangeObserver {
+
+    func registerPhotoLibrary() {
+        PHPhotoLibrary.shared().register(self)
+    }
+
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let asset = fetchResult, let changes = changeInstance.changeDetails(for: asset) else {
+            return
+        }
+
+        fetchResult = changes.fetchResultAfterChanges
+
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
+        }
+    }
+}
