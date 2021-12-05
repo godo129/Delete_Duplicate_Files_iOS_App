c958c2a811223c76a1810a60a4bf3b6f5d55abfd
godo <godo129@naver.com>
2021-12-05 13:31:11 +0900

    아이폰 내부 파일에 저장

--- Eraser_File_App/FileViewController.swift
+++ Eraser_File_App/FileViewController.swift
@@ -7,23 +7,69 @@
 
 import UIKit
 
+struct File {
+    var url: URL
+    var data: Data
+}
+
 class FileViewController: UIViewController {
 
+    
+    let filemg = FileManager.default
+    let path = Bundle.main.resourcePath!
+    
+    var contentLists: [Data] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let file = "\(UUID().uuidString).txt"
+        let contents = "Something happened?"
+        
+        let dir = filemg.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = dir.appendingPathComponent(file)
+        
+        do {
+            try contents.write(to: fileURL, atomically: false, encoding: .utf8)
+        } catch {
+            print("Error: \(error)")
+        }
+        
+        let documnetURL = filemg.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        
+        getData(fileUrl: documnetURL)
+        
+     
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func getData(fileUrl: URL) {
+        
+        print(fileUrl)
+        do {
+            
+            let contensts = try filemg.contentsOfDirectory(at: fileUrl, includingPropertiesForKeys: nil)
+ 
+            for content in contensts {
+                guard let item = filemg.contents(atPath: content.path) else {return}
+                
+                let fileData = File(url: content, data: item)
+                
+                if !contentLists.contains(fileData.data) {
+                    contentLists.append(fileData.data)
+                } else {
+                    
+                    try filemg.removeItem(at: fileData.url)
+                    print(fileData.url)
+                    print(fileData.data)
+                }
+            }
+            
+            print(contentLists)
+            
+        } catch {
+            print("error ocurred ")
+        }
     }
-    */
+
 
 }
