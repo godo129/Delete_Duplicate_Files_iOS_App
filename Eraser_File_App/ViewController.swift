742cfc31f84e1fd984fe7ebf85355b31612445d9
godo <godo129@naver.com>
2021-12-06 23:30:28 +0900

    가지고 있는 이미지 테이블 뷰가 아닌 컬렉션 뷰로 뷰여주기

--- Eraser_File_App/ViewController.swift
+++ Eraser_File_App/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     let tableView = UITableView()
     
-    
+    var collectionVeiw: UICollectionView!
     
     let imageView = UIImageView()
     
@@ -56,15 +56,29 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
-        
-//
+   
 //        cells.dataSource = self
 //        cells.delegate = self
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
 //        cells.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell2")
         
-        view.addSubview(tableView)
+       // view.addSubview(tableView)
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        // 셀 크기 정하기
+        layout.itemSize = CGSize(width: view.frame.size.width/2-2, height: view.frame.size.width/2-2)
+        // 셀마다 공간 정하기
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionVeiw = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        view.addSubview(collectionVeiw)
+        collectionVeiw.register(ImagesCollectionViewCell.self, forCellWithReuseIdentifier: "imageCell")
+        collectionVeiw.dataSource = self
+        collectionVeiw.delegate = self
         
         
         view.addSubview(imageView)
@@ -107,6 +121,7 @@ class ViewController: UIViewController {
         
         tableView.frame = view.bounds
         imageView.frame = CGRect(x: 200, y: 200, width: 300, height: 300)
+        collectionVeiw.frame = view.bounds
         
     }
     
@@ -255,5 +270,28 @@ extension ViewController: PHPhotoLibraryChangeObserver {
         OperationQueue.main.addOperation {
             self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
         }
+    }
+}
+
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchResult.count
+    }
+    
+
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! ImagesCollectionViewCell
+        guard let asset: PHAsset = fetchResult?.object(at: indexPath.row) else {
+            return cell
+        }
+        
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { image, _ in
+            cell.ExerciseImage.image = image
+            cell.ExerciseLabel.isHidden = true 
+        }
+        return cell
     }
 }
