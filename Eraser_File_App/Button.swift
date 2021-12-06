e5ca856d5d1076591c04cf431f9a93eb6bbe22e7
godo <godo129@naver.com>
2021-12-07 05:17:41 +0900

    뒤로 가기 버튼

---
+++ Eraser_File_App/Button.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+let backButton: UIButton = {
+    let backButton = UIButton()
+    backButton.setTitle("Back", for: .normal)
+    backButton.setTitleColor(.black, for: .normal)
+    backButton.frame = CGRect(x: 10, y: 20, width: 50, height: 30)
+    return backButton
+}()
+
+
