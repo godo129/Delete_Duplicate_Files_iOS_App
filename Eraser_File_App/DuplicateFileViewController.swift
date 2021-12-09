45b5f521ebf06dd8cc0de12c27005b66421b57d7
godo <godo129@naver.com>
2021-12-10 06:07:33 +0900

    중복 선택 후 삭제 기능

--- Eraser_File_App/DuplicateFileViewController.swift
+++ Eraser_File_App/DuplicateFileViewController.swift
@@ -10,9 +10,24 @@ import Lottie
 
 class DuplicateFileViewController: UIViewController {
     
+    var arrSelectedIndex = [IndexPath]()
+    var arrSelectedData = [Data]()
+    
     private let filemg = FileManager.default
     
     private var collectionView: UICollectionView!
+    
+    private let deleteButton: UIButton = {
+        let deleteButton = UIButton()
+        deleteButton.setTitle("삭제", for: .normal)
+        deleteButton.setTitleColor(.black, for: .normal)
+        deleteButton.backgroundColor = .white
+        deleteButton.layer.borderWidth = 2
+        deleteButton.layer.borderColor = UIColor.black.cgColor
+        deleteButton.layer.cornerRadius = 10
+        deleteButton.layer.opacity = 0.7
+        return deleteButton
+    }()
     
     private let deleteAllButton: UIButton = {
         let deleteAllButton = UIButton()
@@ -64,15 +79,20 @@ class DuplicateFileViewController: UIViewController {
 //
 //        getData(fileUrl: rootURL)
         
-        deleteAllButton.addTarget(self, action: #selector(deleteButtonTapped), for: .touchUpInside)
+        deleteAllButton.addTarget(self, action: #selector(deleteAllButtonTapped), for: .touchUpInside)
         
         if duplicateFileData.isEmpty {
             emptyAnimaton.isHidden = false
             deleteAllButton.isHidden = true
+            deleteButton.isHidden = true
         } else {
             emptyAnimaton.isHidden = true
             deleteAllButton.isHidden = false
+            deleteButton.isHidden = false
         }
+        
+        view.addSubview(deleteButton)
+        deleteButton.addTarget(self, action: #selector(deleteButtonTapped), for: .touchUpInside)
 
     }
     
@@ -87,6 +107,60 @@ class DuplicateFileViewController: UIViewController {
     }
     
     @objc private func deleteButtonTapped() {
+        
+        let alert = UIAlertController(title: "", message: "삭제하시겠습니까?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "네", style: .default, handler: { _ in
+            
+            self.arrSelectedIndex.sort(by: >)
+            for indexPath in self.arrSelectedIndex {
+                
+                let toDeleteURLs = duplicateFileLists[duplicateFileData[indexPath.row]]!
+            
+                duplicateFileCount -= toDeleteURLs.count
+            
+                for toDeleteURL in toDeleteURLs {
+                    do{
+                        try self.filemg.removeItem(at: toDeleteURL)
+            
+                    } catch {
+                        print("delete errored")
+                    }
+                }
+            
+                duplicateFileLists.removeValue(forKey: duplicateFileData[indexPath.row])
+            
+                var empty: [URL] = []
+            
+                duplicateFileLists[duplicateFileData[indexPath.row]] = empty
+            
+                duplicateFileData.remove(at: indexPath.row)
+            
+            
+                self.collectionView.deleteItems(at: [indexPath])
+            
+                self.collectionView.reloadData()
+            
+                if duplicateFileData.isEmpty {
+                    self.emptyAnimaton.isHidden = false
+                    self.deleteAllButton.isHidden = true
+                    self.deleteButton.isHidden = true
+                }
+                
+                self.arrSelectedData.removeAll()
+                self.arrSelectedIndex.removeAll()
+            }}))
+  
+        alert.addAction(UIAlertAction(title: "아니요", style: .cancel, handler: nil))
+        
+        present(alert, animated: false, completion: nil)
+        
+
+        
+        
+        
+    }
+    
+    @objc private func deleteAllButtonTapped() {
         
         let alert = UIAlertController(title: "", message: "정말로 삭제하시겠습니까?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
@@ -208,6 +282,7 @@ class DuplicateFileViewController: UIViewController {
         
         emptyAnimaton.frame = view.bounds
         deleteAllButton.frame = CGRect(x: view.frame.width-100, y: 0, width: 100, height: 40)
+        deleteButton.frame = CGRect(x: deleteAllButton.frame.origin.x-100, y: 0, width: 100, height: 40)
     }
     
 
@@ -223,54 +298,79 @@ extension DuplicateFileViewController: UICollectionViewDataSource, UICollectionV
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "fileCell", for: indexPath) as! ImagesCollectionViewCell
         
+        if arrSelectedIndex.contains(indexPath) {
+            cell.selectedIndicator.isHidden = false
+            
+        } else {
+            cell.selectedIndicator.isHidden = true
+        }
+        
         cell.ExerciseImage.image = UIImage(data: duplicateFileData[indexPath.row])
         let fileName = duplicateFileLists[duplicateFileData[indexPath.row]]![0].lastPathComponent.description
         cell.ExerciseLabel.text = "\(fileName) \(duplicateFileLists[duplicateFileData[indexPath.row]]!.count)"
         
+        cell.layer.borderWidth = 2
+        cell.layer.borderColor = UIColor.black.cgColor
+        
         return cell
     }
 
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        
+//        let alert = UIAlertController(title: "", message: "삭제하시겠습니까?", preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "네", style: .default, handler: { _ in
+//            let toDeleteURLs = duplicateFileLists[duplicateFileData[indexPath.row]]!
+//            
+//            duplicateFileCount -= toDeleteURLs.count
+//            
+//            for toDeleteURL in toDeleteURLs {
+//                do{
+//                    try self.filemg.removeItem(at: toDeleteURL)
+//                    
+//                } catch {
+//                    print("delete errored")
+//                }
+//            }
+//            
+//            duplicateFileLists.removeValue(forKey: duplicateFileData[indexPath.row])
+//            
+//            var empty: [URL] = []
+//            
+//            duplicateFileLists[duplicateFileData[indexPath.row]] = empty
+//            
+//            duplicateFileData.remove(at: indexPath.row)
+//            
+//            
+//            collectionView.deleteItems(at: [indexPath])
+//            
+//            collectionView.reloadData()
+//            
+//            if duplicateFileData.isEmpty {
+//                self.emptyAnimaton.isHidden = false
+//                self.deleteAllButton.isHidden = true
+//            }
+//        }))
+//        alert.addAction(UIAlertAction(title: "아니요", style: .cancel, handler: nil))
+//        
+//        present(alert, animated: false, completion: nil)
+//        
+//        
+//
+//    }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("you selected cell \(indexPath.item)")
         
-        let alert = UIAlertController(title: "", message: "삭제하시겠습니까?", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "네", style: .default, handler: { _ in
-            let toDeleteURLs = duplicateFileLists[duplicateFileData[indexPath.row]]!
-            
-            duplicateFileCount -= toDeleteURLs.count
-            
-            for toDeleteURL in toDeleteURLs {
-                do{
-                    try self.filemg.removeItem(at: toDeleteURL)
-                    
-                } catch {
-                    print("delete errored")
-                }
-            }
-            
-            duplicateFileLists.removeValue(forKey: duplicateFileData[indexPath.row])
-            
-            var empty: [URL] = []
-            
-            duplicateFileLists[duplicateFileData[indexPath.row]] = empty
-            
-            duplicateFileData.remove(at: indexPath.row)
-            
-            
-            collectionView.deleteItems(at: [indexPath])
-            
-            collectionView.reloadData()
-            
-            if duplicateFileData.isEmpty {
-                self.emptyAnimaton.isHidden = false
-                self.deleteAllButton.isHidden = true
-            }
-        }))
-        alert.addAction(UIAlertAction(title: "아니요", style: .cancel, handler: nil))
+        let strData = duplicateFileData[indexPath.row]
         
-        present(alert, animated: false, completion: nil)
-        
-        
-
+        if arrSelectedIndex.contains(indexPath) {
+            arrSelectedIndex = arrSelectedIndex.filter{$0 != indexPath}
+            arrSelectedData = arrSelectedData.filter{$0 != strData}
+        } else {
+            arrSelectedIndex.append(indexPath)
+            arrSelectedData.append(strData)
+        }
+        collectionView.reloadData()
     }
     
 }
--- Eraser_File_App/DuplicateImageViewController.swift
+++ Eraser_File_App/DuplicateImageViewController.swift
@@ -94,8 +94,6 @@ class DuplicateImageViewController: UIViewController {
             deleteButton.isHidden = false
         }
         
-        print(duplicateLists)
-        print(duplicateImageData)
         
         collectionView.allowsSelection = true
 
@@ -281,6 +279,9 @@ extension DuplicateImageViewController: UICollectionViewDataSource, UICollection
         
         cell.ExerciseImage.image = UIImage(data: duplicateImageData[indexPath.row])!
         cell.ExerciseLabel.text = "\(duplicateLists[duplicateImageData[indexPath.row]]!.count)"
+        
+        cell.layer.borderWidth = 2
+        cell.layer.borderColor = UIColor.black.cgColor
         
         return cell
     }
